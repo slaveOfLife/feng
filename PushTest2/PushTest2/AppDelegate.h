fd360398f90296a94c265a73f2602d8c43d9ad43
尹齐峰 <feng@yinqifeng.local>
2016-08-24 18:42:38 +0800

    init

---
+++ PushTest2/PushTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PushTest2
+//
+//  Created by feng on 16/8/24.
+//  Copyright © 2016年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
